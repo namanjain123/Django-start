79e6f6e2ac842b544c670a846067f8afb3863a05
naman jain <naman.nishi@gmail.com>
2019-09-18 11:42:32 +0530

    new file of bootstrap and javascript

---
+++ first.js
@@ -0,0 +1,5 @@
+alert("WelcomeTo your weight converter");
+var deposit= prompt("Enter your weight in pounds");
+deposit=deposit*.454;
+alert("you have weight in kg as:"+deposit)
+console.log("you are a cooll person");
---
+++ two.js
@@ -0,0 +1,11 @@
+alert("hello");
+var hot=false
+var temp=prompt("enter the temprature of today in fahranite")
+if(temp>80){
+  hot=true
+  console.log("It runs and temprature is greater than 80");
+}
+else {
+  hot =flase
+  console.console.log("not that hot outside");
+}
